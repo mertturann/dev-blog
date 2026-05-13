15479ed066a5be84264c017aba145df342be4e75
mertturann <linuxturkey06@gmail.com>
2026-05-14 02:13:36 +0300

    add: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from "@/lib/theme";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+
+const GA_ID = "G-VT8CSELFSN";
 
 const geistSans = Geist({ variable: "--font-sans", subsets: ["latin"], display: "swap" });
 const geistMono = Geist_Mono({ variable: "--font-mono", subsets: ["latin"], display: "swap" });
@@ -14,6 +17,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		>
 			<body>
 				<ThemeProvider>{children}</ThemeProvider>
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+					strategy="afterInteractive"
+				/>
+				<Script id="gtag-init" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', '${GA_ID}');
+					`}
+				</Script>
 			</body>
 		</html>
 	);
